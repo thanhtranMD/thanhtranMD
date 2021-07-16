6f21428f04d6a0cfbbf970c17185f7d8ca237d85
thanhtranMD <thanh.tran@medicaldirector.com>
2021-07-17 09:55:59 +1000

    implement a main menu selection for ease addon new examples

--- DataParallelSolution/SimpleParallelForEach/Program.cs
+++ DataParallelSolution/SimpleParallelForEach/Program.cs
@@ -12,12 +12,32 @@ namespace SimpleParallelForEach
     {
         static void Main(string[] args)
         {
-            //String[] args = Environment.GetCommandLineArgs();
-            Console.WriteLine("MShow directory size example");
-            DirectorySizeExample.ShowDirectoryTotalSize();
-
-            Console.WriteLine("Multiply Matrices example");
-            MultiplyMatrices.ExecMultiplyMatrices(args);
+            const string welcomeText = "Welcome to Task Parallel Library Examples";
+            Console.WriteLine(welcomeText);
+            Console.WriteLine("-".PadRight(welcomeText.Length));
+            string selectedMenu;
+            do
+            {
+                Console.WriteLine("Select a menu:");
+                Console.WriteLine("1 - MShow directory size example");
+                Console.WriteLine("2 - Multiply Matrices example");
+                Console.WriteLine("Q or q to Quit.");
+                selectedMenu = Console.ReadLine();
+                switch (selectedMenu)
+                {
+                    case "1":
+                        DirectorySizeExample.ShowDirectoryTotalSize();
+                        break;
+                    case "2":
+                        MultiplyMatrices.ExecMultiplyMatrices(args);
+                        break;
+                    default:
+                        Console.WriteLine("Invalid selection.");
+                        break;
+                }
+            } while (selectedMenu.ToLower() != "q");
+            Console.WriteLine("Enter any key to quit.");
+            Console.ReadLine();
         }
 
 
